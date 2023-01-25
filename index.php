8ac7754b1fa2f4b5862bdcb17e540f656feed9f4
Ernandes R de Souza <70029077+ernandesrs@users.noreply.github.com>
2023-01-25 16:00:36 -0400

    Updating section css. Create portfolio section.

--- index.php
+++ index.php
@@ -15,7 +15,8 @@ require __DIR__ . "/app/config.php";
 
     <link rel="stylesheet" href="<?= CONF_APP_ASSETS_URL ?>/css/icons.css">
     <link rel="stylesheet" href="<?= CONF_APP_ASSETS_URL ?>/css/styles.css">
-    <link rel="shortcut icon" href="<?= CONF_APP_ASSETS_URL ?>/img/favicon.png" type="image/x-icon">
+    <link rel="shortcut icon" href="<?= CONF_APP_ASSETS_URL ?>/img/favicon.png"
+        type="image/x-icon">
 </head>
 
 <body>
@@ -26,10 +27,12 @@ require __DIR__ . "/app/config.php";
             <div class="container">
                 <nav class="d-flex align-items-center">
                     <a class="logo" href="">
-                        <img class="img-fluid" src="<?= CONF_APP_ASSETS_URL ?>/img/logo.png" alt="ERSOUZA LOGO">
+                        <img class="img-fluid"
+                            src="<?= CONF_APP_ASSETS_URL ?>/img/logo.png"
+                            alt="ERSOUZA LOGO">
                     </a>
 
-                    <ul class="ms-auto nav flex-row">
+                    <ul class="ms-auto nav flex-row d-none d-lg-flex">
                         <?php foreach (CONF_APP_DATA["header"]["nav"] ?? [] as $nav): ?>
                             <li class="nav-item">
                                 <a href="<?= $nav["url"] ?>"
@@ -42,6 +45,11 @@ require __DIR__ . "/app/config.php";
                             </li>
                         <?php endforeach; ?>
                     </ul>
+
+                    <button
+                        class="btn px-0 text-light-light fs-4 ms-auto d-lg-none bg-transparent">
+                        <i class="bi bi-list"></i>
+                    </button>
                 </nav>
             </div>
         </header>
@@ -54,7 +62,7 @@ require __DIR__ . "/app/config.php";
                 </div>
 
                 <div class="col-12 col-lg-6 text-center text-lg-start ps-lg-2">
-                    <h1 class="section-title">
+                    <h1 class="section-title typing">
                         <?= CONF_APP_DATA["banner"]["title"] ?>
                     </h1>
                     <p class="py-3 banner-desc">
@@ -79,8 +87,99 @@ require __DIR__ . "/app/config.php";
 
     <!-- skills -->
     <section class="section section-skills">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-12 col-lg-10">
+                    <div class="row pb-3 mb-md-4">
+                        <div
+                            class="col-12 col-md-11 col-lg-10 col-xl-8 text-center text-md-start">
+                            <h1 class="section-title typing pe-5 pe-xl-4">
+                                <?= CONF_APP_DATA["skills"]["title"] ?>
+                            </h1>
+                        </div>
+                    </div>
+
+                    <div class="row justify-content-center">
+                        <?php foreach (CONF_APP_DATA["skills"]["skills"] as $skill): ?>
+                            <div class="col-12 col-sm-6 col-lg-4 mb-3">
+                                <div class="card card-body h-100 skill-box">
+                                    <div class="skill-icon-box">
+                                        <i class="icon <?= $skill["icon"] ?>"></i>
+                                    </div>
+                                    <div class="skill-content">
+                                        <h2 class="skill-title">
+                                            <?= $skill["title"] ?>
+                                        </h2>
+                                        <p class="skill-desc">
+                                            <?= $skill["desc"] ?>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        <?php endforeach; ?>
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
     <!-- /skills -->
+
+    <!-- portfolio -->
+    <section class="section section-portfolio">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-12 col-lg-10">
+                    <div class="row justify-content-md-end pb-3 mb-md-4">
+                        <div
+                            class="col-12 col-md-11 col-lg-10 col-xl-6 text-center text-md-end">
+                            <h1 class="section-title typing ps-5 ps-xl-4">
+                                <?= CONF_APP_DATA["portfolio"]["title"] ?>
+                            </h1>
+                        </div>
+                    </div>
+
+                    <div class="row justify-content-center">
+                        <?php foreach (CONF_APP_DATA["portfolio"]["jobs"] as $job): ?>
+                            <div class="col-12 col-sm-6 col-lg-4 mb-3">
+                                <div class="card card-body h-100 job-box">
+                                    <div class="job-image-box">
+                                        <img class="img-fluid" src="<?= $job["image"] ?>"
+                                            alt="<?= $job["title"] ?> Image">
+
+                                        <div class="py-2 job-links">
+                                            <?php if ($url = $job["preview_url"]): ?>
+                                                <a href="<?= $url ?>" title="Previsualizar"
+                                                    target="_blank">
+                                                    <i class="bi bi-eye-fill"></i>
+                                                    <span class="text">Preview</span>
+                                                </a>
+                                            <?php endif; ?>
+                                            <?php if ($url = $job["source_url"]): ?>
+                                                <a href="<?= $url ?>" title="Código fonte"
+                                                    target="_blank">
+                                                    <i class="bi bi-github"></i>
+                                                    <span class="text">Fonte</span>
+                                                </a>
+                                            <?php endif; ?>
+                                        </div>
+                                    </div>
+                                    <div class="job-content">
+                                        <h2 class="job-title">
+                                            <?= $job["title"] ?>
+                                        </h2>
+                                        <p class="job-desc">
+                                            <?= $job["desc"] ?>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        <?php endforeach; ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- /portfolio -->
 
     <script src="<?= CONF_APP_ASSETS_URL ?>/js/scripts.js"></script>
 </body>
